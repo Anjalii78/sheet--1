7a1f2ef74e1908b0528d021f29453df136e54dc5
~ANJALI~ <anjalikumari15243@gmail.com>
2025-09-01 21:54:33 -0500

    Update p12_integers.py

--- p12_integers.py
+++ p12_integers.py
@@ -1,10 +1,11 @@
-A = int(input("Enter A: "))
-B = int(input("Enter B: "))
-result = 1
+A = int(input("Enter A : "))
+B = int(input("Enter B : "))
 
-for i in range(B):
-    result *= A
-
-print("Result:", result)
+result = 1  
+i = 1
+while i <= B:
+    result *= A  
+    i += 1
+print("result =", result)
 
 #you are given two integers A and B. you have to find the value of A^B.
