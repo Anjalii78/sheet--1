1d821fff09d906a785cfffa6512701b1d2cbffd2
~ANJALI~ <anjalikumari15243@gmail.com>
2025-09-01 21:52:20 -0500

    Update p7_sumodd.py

--- p7_sumodd.py
+++ p7_sumodd.py
@@ -1,9 +1,12 @@
+A = int(input("Enter a no. : "))
 
-A = int(input("Enter a number: "))
-odd_sum = 0
-for i in range(1, A+1):
-    if i % 2 != 0:
-        odd_sum += i
-print("Sum of odd numbers:", odd_sum)
+i = 1
+sum = 0
+while i <= A :
+    if i%2 != 0 :
+        sum += i
+    i += 1
+print(sum)
+
 
 #take an integer A as input.you have to print the sum of all odd number in the range[1,A]
