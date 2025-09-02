6121b9188e688646297f542e940c6b468f5934e6
~ANJALI~ <anjalikumari15243@gmail.com>
2025-09-01 21:55:42 -0500

    Update p10_palindrome.py

--- p10_palindrome.py
+++ p10_palindrome.py
@@ -1,17 +1,17 @@
-A = int(input("Enter a number: "))
-original = A
-reverse = 0
+n = int(input("Enter a no. n : "))
+reverseNum = 0
+myNum = n
 
-while A > 0:
-    digit = A % 10
-    reverse = reverse * 10 + digit
-    A //= 10
-
-if original == reverse:
-    print("Yes")   
+while(n > 0) :
+    last_digit = n % 10
+    reverseNum = reverseNum * 10 + last_digit
+    n = n//10
+if(myNum  == reverseNum):
+    print(myNum,"=",reverseNum,"This no. is a Palindrome")
 else:
-    print("No")    
+    print("no.",myNum,"is NOT a palindrome")
     
 #You are given an integer A as input, and you need to determine whether it is a palindrome
 #or not. A palindrome integer is one whose digits, when reversed, result in the same number.
+
  
